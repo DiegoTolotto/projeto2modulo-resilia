9ae98aec96bcf5567629269aea76863d749baf23
Diego Tolotto <diego.tolotto8@gmail.com>
2022-02-14 14:07:11 -0300

    feat: adicionando animação dos containers

--- home.js
+++ home.js
@@ -1,3 +1,53 @@
+
+// função da animação
+// função da animação
+// função da animação
+
+const debounce = function (func, wait, immediate) {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout (timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args)
+    };
+};
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = "animate";
+
+function animeScroll () {
+    const rolando = window.scrollY + ((window.innerHeight * 3) / 4);
+    target.forEach(function(elemento){
+        if ((rolando) > elemento.offsetTop) {
+            elemento.classList.add(animationClass)
+        } else {
+            elemento.classList.remove(animationClass)
+        }
+    })
+}
+
+animeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll', function (){
+        animeScroll();
+    })
+} 
+
+
+
+
+// função da textarea
+// função da textarea
+// função da textarea
+
 $('#fun').on("click", () => {
     $('#textarea-primeira-informacao').html(`function nomeDaFuncao (parametro) { 
         let arrayVazia = [] 
